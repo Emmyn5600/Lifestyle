28bcc6fcd7b839197dc056654e0dd686fadd110d
emmy <emmy66418@gmail.com>
2021-05-17 14:21:34 +0200

    Validate our tables inside the models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,9 @@
 class Category < ApplicationRecord
+  has_many :companies, foreign_key: :category_id
+  has_many :articles, -> { order(id: 'desc') }, through: :companies
+
+  validates :name, presence: true, length: { minimum: 5, maximum: 20 }, uniqueness: true
+
+  validates :priority, presence: true, uniqueness: true
+  
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :article
+
+  validates :body, presence: true, length: { minimum: 5, maximum: 1000 }
 end
