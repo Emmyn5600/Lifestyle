6a210294a05d1df832c8633942b66c510d3880ea
emmy <emmy66418@gmail.com>
2021-05-17 12:54:10 +0200

    connect the postgresql with the application

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_05_17_103658) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
