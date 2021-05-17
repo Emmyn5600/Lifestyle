8236d621c0d077eb2c2c840de4f7d6d26a66c653
emmy <emmy66418@gmail.com>
2021-05-17 17:30:14 +0200

    work on the login page

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,42 @@
+class CategoriesController < ApplicationController
+    before_action :authorize, only: %i[index]
+  
+    def index
+      @categories = Category.order('priority ASC').includes(:articles).select do |e|
+        e.articles.count.positive?
+      end
+      if Vote.count.positive?
+        @vote = Vote.all.group(:article_id).count.max_by { |_k, v| v }.first
+        @article = Article.find(@vote)
+      elsif Article.count.positive?
+        @article = Article.first
+      else
+        redirect_to new_category_path
+      end
+    end
+  
+    def new
+      @category = Category.new
+    end
+  
+    def create
+      @category = Category.new(category_params)
+      if @category.save
+        flash.notice = 'Category created!'
+        redirect_to root_path
+      else
+        render :new
+      end
+    end
+  
+    def show
+      @category = Category.includes(articles: :author).find(params[:id])
+    end
+  
+    private
+  
+    def category_params
+      params.require(:category).permit(:name, :priority)
+    end
+  end
+  
