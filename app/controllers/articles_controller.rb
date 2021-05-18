5cc06cd11a7e16abd08378626bee4ddf15919f9a
emmy <emmy66418@gmail.com>
2021-05-18 17:39:14 +0200

    Add the articles controller functionalities

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,31 @@
 class ArticlesController < ApplicationController
+    def index; end
+
+    def new
+        @article = current_user.articles.build
+    end
+
+    def create
+        @article = current_user.articles.build(articles_params)
+
+        if articles.save
+            flash.notice = 'Article created successfully!'
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+
+     def show
+        @article = Article.includes(:comments, :user).find(params[:id])
+        @comment = Comment.new
+        @comment.article_id = @article_id
+     end
+
+     private
+
+     def articles_params
+     params.require(:article).permit(:title, :image, :category_ids: [])
+     end
+
 end
