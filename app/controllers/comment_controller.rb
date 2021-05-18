9b1eece8348d6853174e75fd10d0c87cdead824a
emmy <emmy66418@gmail.com>
2021-05-18 19:46:21 +0200

    Work on the comment controller section

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,2 +1,19 @@
 class CommentController < ApplicationController
+
+    before_action :authenticate, only: %i[create]
+
+    def create
+        @comment = Comment.new(comment_params)
+        @comment.article_id = params[:article_id]
+        @comment.user_id = current_user.id
+
+        @comment.save
+        flash.notice = 'Your comment has been added to this article'
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:body)
+    end
 end
