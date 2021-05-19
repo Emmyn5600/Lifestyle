91842d39dd5cd7a9f21c2700a8f7ef32cf6cb9eb
emmy <emmy66418@gmail.com>
2021-05-19 15:44:31 +0200

    Modify the article model and view

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,6 +7,6 @@ class Article < ApplicationRecord
   has_many :comments
 
   validates :title, presence: true, length: { minimum: 5, maximum: 50 }
-  validates :text, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :text, presence: true, length: { minimum: 5, maximum: 1000 }
   validates :image, presence: true
 end
