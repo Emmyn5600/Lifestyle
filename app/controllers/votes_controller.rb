6ef01b4a8ed32070805ff0b62a3fec09e46722f9
emmy <emmy66418@gmail.com>
2021-05-19 11:39:32 +0200

    Add the votes controller logic

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,10 @@
 class VotesController < ApplicationController
+ before_action :authenticate, only: %i[add_vote]
+ 
+ def add_vote
+    @article = Article.find(params[:article_id])
+    current_user.votes.create(article: @article)
+    flash.notice = 'Vote added successfully!'
+    redirect_back(fallback_location: root_path)
+ end
 end
