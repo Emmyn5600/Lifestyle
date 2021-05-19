09cd5cf946105a78a337c1c4e8784886ba909308
emmy <emmy66418@gmail.com>
2021-05-19 14:22:57 +0200

    Add the comment form and modify controller for the logic

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,4 @@
-class CommentController < ApplicationController
+class CommentsController < ApplicationController
 
     before_action :authenticate, only: %i[create]
 
@@ -6,14 +6,15 @@ class CommentController < ApplicationController
         @comment = Comment.new(comment_params)
         @comment.article_id = params[:article_id]
         @comment.user_id = current_user.id
-
+    
         @comment.save
-        flash.notice = 'Your comment has been added to this article'
-    end
-
-    private
-
-    def comment_params
+        flash.notice = 'Your comment has been added'
+        redirect_to article_path(@comment.article)
+      end
+    
+      private
+    
+      def comment_params
         params.require(:comment).permit(:body)
-    end
+      end
 end
