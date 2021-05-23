7f2a89f28b91bed208198b49bfd868983ca58742
emmy <emmy66418@gmail.com>
2021-05-23 16:49:32 +0200

    Add amazon credential for deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store files on Amazon S3.
-  config.active_storage.service = :Amazon
+  config.active_storage.service = :amazon
 
 
   # Mount Action Cable outside main process or domain.
